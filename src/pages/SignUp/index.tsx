935ff46b20df19411d2fca6cf88d1357d1835a1b
GabrielGomes25 <gabriel.gs25@gmail.com>
2020-07-20 16:18:19 -0300

    initial commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,146 @@
+import React, { useRef, useCallback } from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import api from '../../services/api';
+import {
+  Container,
+  Title,
+  BackToSignIn,
+  BackToSignInText
+} from './style';
+import Input from '../../components/input';
+import Button from '../../components/button';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+
+
+import logoImg from '../../assets/logo.png';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const handleSingUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      await api.post('/users',data);
+
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação!',
+        );
+
+      navigation.goBack();
+    }
+    catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+      Alert.alert(
+        'Erro na autenticação',
+        'Ocorreu um erro ao fazer cadastro, tente novamente. ',
+      );
+    }
+  }, [navigation]);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie a sua conta</Title>
+            </View>
+            <Form
+              ref={formRef}
+              onSubmit={handleSingUp}
+              >
+
+              <Input
+                autoCapitalize="words"
+                name='name'
+                icon='user'
+                placeholder='Nome'
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name='email'
+                icon='mail'
+                placeholder='E-mail'
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                name='password'
+                icon='lock'
+                placeholder='Senha'
+                textContentType="newPassword"
+              />
+            </Form>
+            <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn onPress={() => { navigation.navigate('SignIn') }}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
+};
+
+export default SignUp;
